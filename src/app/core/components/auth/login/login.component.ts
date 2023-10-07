cfdbb579ce11cac1a00ad8bc082c9a67f52a5c61
vargbalazs <balazs.varga.8564@gmail.com>
2023-10-07 20:45:29 +0200

    building login component 1

--- src/app/core/components/auth/login/login.component.ts
+++ src/app/core/components/auth/login/login.component.ts
@@ -1,8 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
+  styleUrls: ['./login.component.css', '../authroot/authroot.component.css'],
 })
-export class LoginComponent {}
+export class LoginComponent implements OnInit {
+  loginForm!: ReturnType<typeof this.initLoginForm>;
+
+  ngOnInit(): void {
+    this.loginForm = this.initLoginForm();
+  }
+
+  initLoginForm() {
+    return new FormGroup({
+      userEmail: new FormControl('', {
+        validators: [Validators.required, Validators.email],
+        updateOn: 'submit',
+      }),
+      password: new FormControl('', {
+        validators: [Validators.required],
+        updateOn: 'submit',
+      }),
+      rememberMe: new FormControl(),
+    });
+  }
+
+  submitForm() {
+    this.loginForm.markAllAsTouched();
+    if (this.loginForm.valid) {
+      const user = this.loginForm.value;
+    }
+  }
+}
