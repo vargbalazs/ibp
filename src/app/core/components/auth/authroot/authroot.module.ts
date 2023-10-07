d1c1b8156689cbeca22e46c210e62211eeb5656e
vargbalazs <balazs.varga.8564@gmail.com>
2023-10-07 16:25:08 +0200

    authroot layout

--- src/app/core/components/auth/authroot/authroot.module.ts
+++ src/app/core/components/auth/authroot/authroot.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthRootRoutingModule } from './authroot-routing.module';
 import { AuthrootComponent } from './authroot.component';
+import { LayoutModule } from '@progress/kendo-angular-layout';
 
 @NgModule({
   declarations: [AuthrootComponent],
-  imports: [CommonModule, AuthRootRoutingModule],
+  imports: [CommonModule, AuthRootRoutingModule, LayoutModule],
   exports: [AuthrootComponent],
 })
 export class AuthrootModule {}
