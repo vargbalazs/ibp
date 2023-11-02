24193860fff32b4b928c77b48a6f44ce5aff1647
vargbalazs <balazs.varga.8564@gmail.com>
2023-11-02 12:19:49 +0100

    adding action feature

--- src/app/features/admin/admin.module.ts
+++ src/app/features/admin/admin.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RootModule } from './components/root/root.module';
+import { ActionModule } from './components/action/action..module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, RootModule],
+  imports: [CommonModule, RootModule, ActionModule],
   providers: [],
   exports: [],
 })
