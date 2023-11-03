6b452bf64f31ee413b27f822bbfd3f6ae8256421
vargbalazs <balazs.varga.8564@gmail.com>
2023-11-03 13:43:53 +0100

    adding generic create-edit component

---
+++ src/app/shared/components/create-edit/create-edit.component.ts
@@ -0,0 +1,47 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+@Component({
+  template: '',
+})
+export class CreateEditComponent<T extends { id: number }> {
+  formData: T = <T>{};
+  form: FormGroup = new FormGroup({});
+
+  active = false;
+  editMode = false;
+  comboBoxValueChange = false;
+
+  @Input() isNew = false;
+  @Input() set model(entity: T) {
+    this.form.reset(entity);
+    this.active = entity !== undefined;
+    this.editMode = entity?.id ? true : false;
+  }
+
+  @Output() save: EventEmitter<T> = new EventEmitter();
+  @Output() cancel: EventEmitter<any> = new EventEmitter();
+
+  onSave() {
+    this.form.markAllAsTouched();
+    if (this.form.valid) {
+      this.active = false;
+      this.save.emit(this.form.value);
+      this.resetState();
+    }
+  }
+
+  onCancel() {
+    this.resetState();
+    this.closeForm();
+  }
+
+  closeForm() {
+    this.resetState();
+    this.active = false;
+    this.cancel.emit();
+  }
+
+  // will be overridden in the derived component - cannot be generic
+  resetState() {}
+}
