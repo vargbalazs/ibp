48aa0911e665bdae2901c5500461d26b99667914
vargbalazs <balazs.varga.8564@gmail.com>
2023-11-18 08:59:18 +0100

    add custom notify compact comp

---
+++ src/app/shared/components/custom-notify-compact/custom-notify-compact.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'custom-notify-compact',
+  templateUrl: './custom-notify-compact.component.html',
+  styleUrls: ['./custom-notify-compact.component.css'],
+})
+export class CustomNotifyCompactComponent {
+  iconClass = '';
+  barClass = '';
+  _type = '';
+
+  @Input() title = '';
+  @Input() descr = '';
+  @Input() set type(value: string) {
+    this.iconClass = 'custom-notification-compact' + value;
+    this.barClass = 'custom-notification-compact-bar-' + value;
+    this._type = value;
+  }
+  get type(): string {
+    return this._type;
+  }
+
+  @Output() public close: EventEmitter<unknown> = new EventEmitter();
+
+  closeNotification(event: Event): void {
+    event.preventDefault();
+    this.close.emit();
+  }
+}
---
+++ src/app/shared/components/custom-notify-compact/custom-notify-compact.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CustomNotifyCompactComponent } from './custom-notify-compact.component';
+
+@NgModule({
+  declarations: [CustomNotifyCompactComponent],
+  imports: [CommonModule],
+  exports: [CustomNotifyCompactComponent],
+})
+export class CustomNofityCompactModule {}
