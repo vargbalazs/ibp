82004f2e03e5913a9f624d169a5d9e929af2a449
vargbalazs <balazs.varga.8564@gmail.com>
2023-11-30 14:27:28 +0100

    adding masterdata routing

--- src/app/features/features-routing.module.ts
+++ src/app/features/features-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin/admin-routing.module';
+import { MasterDataRoutingModule } from './masterdata/masterdata-routing.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, AdminRoutingModule],
+  imports: [CommonModule, AdminRoutingModule, MasterDataRoutingModule],
   providers: [],
   exports: [],
 })
---
+++ src/app/features/masterdata/masterdata-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from 'src/app/core/components/layout/layout.component';
+import { BuListComponent } from './components/bu/list/list.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'masterdata/bus',
+        component: BuListComponent,
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class MasterDataRoutingModule {}
