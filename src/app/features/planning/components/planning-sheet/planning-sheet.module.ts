e3051f29b37c7acb6020e43b5ef9dc18d9e6b068
vargbalazs <balazs.varga.8564@gmail.com>
2023-12-19 18:29:59 +0100

    adding syncfusion spreadsheet

--- src/app/features/planning/components/planning-sheet/planning-sheet.module.ts
+++ src/app/features/planning/components/planning-sheet/planning-sheet.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PlanningSheetComponent } from './planning-sheet.component';
+import { SpreadsheetAllModule } from '@syncfusion/ej2-angular-spreadsheet';
 
 @NgModule({
   declarations: [PlanningSheetComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, SpreadsheetAllModule],
   exports: [PlanningSheetComponent],
 })
 export class PlanningSheetModule {}
